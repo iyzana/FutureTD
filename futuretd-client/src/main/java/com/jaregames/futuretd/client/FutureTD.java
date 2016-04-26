5aee593036ae7557063bdbb40705c4af1ac18b7e
Unh0ld <r.brinkhege@osnanet.de>
2016-04-26 21:36:29 +0200

    General layout for the Main Window

---
+++ futuretd-client/src/main/java/com/jaregames/futuretd/client/FutureTD.java
@@ -0,0 +1,10 @@
+package com.jaregames.futuretd.client;
+
+/**
+ * Created by René on 26.04.2016.
+ */
+public class FutureTD {
+    public static void main(String[] args) {
+        new GameWindow();
+    }
+}
--- futuretd-client/src/main/java/com/jaregames/futuretd/client/GameWindow.java
+++ futuretd-client/src/main/java/com/jaregames/futuretd/client/GameWindow.java
@@ -1,11 +1,25 @@
 package com.jaregames.futuretd.client;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
 /**
- * Project: futuretd
- * <p/>
- * Created on 26.04.2016 at 21:25
- *
- * @author Jannis
+ * Created by René on 26.04.2016.
  */
-public class GameWindow {
+class GameWindow {
+    private JFrame gameWindow;
+    private JPanel gamePanel;
+    GameWindow() {
+        gamePanel = new JPanel();
+        gamePanel.setSize(1000, 800);
+        gamePanel.setVisible(true);
+
+        gameWindow = new JFrame("FutureTD");
+        gameWindow.setSize(1000, 800);
+        gameWindow.setLocationRelativeTo(null);
+        gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        gameWindow.add(gamePanel);
+        gameWindow.setVisible(true);
+    }
 }
